bd2ba19e7736bf36eba0a2ed7648b1e6cfbd4fab
gmorais <gmorais@fatecie.edu.br>
2026-04-17 23:26:22 -0300

    feat(login): refatorar página de login e adicionar novos componentes de botão e input

--- app/(unauthenticated)/login/page.tsx
+++ app/(unauthenticated)/login/page.tsx
@@ -1,19 +1,25 @@
 import { Column } from "@/components/column";
 import { Row } from "@/components/row";
-import { EyeIcon } from "lucide-react";
-import { FaShieldHalved } from "react-icons/fa6";
-import Input from "./components/input";
+import { FaArrowRight, FaShieldHalved } from "react-icons/fa6";
+import Input from "../../../components/input";
+import { Button } from "@/components/button";
 
 export default function Login() {
   return (
-    <Column className="grow uppercase bg-linear-to-br from-brand-bege-claro font-grotesk from-50% to-brand-bege-escuro items-center justify-center">
-      <Column className="gap-14 items-center">
-        <p className="pl-6 pr-5 italic py-3 bg-brand-secondary text-center font-bold shadow-hard text-6xl -rotate-4">
-          LIVEFLIP
+    <Column className="gap-14 pb-10 items-center overflow-hidden  grow uppercase bg-linear-to-br from-brand-secondary font-grotesk to-neutral-80 justify-center">
+      <p className="pl-6 pr-5 italic py-3 bg-brand-primary text-center font-bold shadow-hard text-6xl -rotate-4">
+        LIVEFLIP
+      </p>
+      <div className="relative">
+        <p className="absolute text-[10rem] font-bold text-black -left-65 bottom-110 opacity-10">
+          FLIP
         </p>
-        <Column className="bg-neutral-900 text-gray-300 gap-10 p-10 border-4 border-neutral-950 w-xl">
-          <Column className="gap-1">
-            <Row className="text-xs text-brand-secondary font-bold">
+        <p className="absolute text-[10rem] font-bold text-black -right-65 top-110 opacity-10">
+          LIVE
+        </p>
+        <Column className="bg-neutral-900 text-gray-300 relative gap-10 p-10 border-4 border-neutral-950 w-xl z-20">
+          <Column className="gap-3">
+            <Row className="text-xs text-brand-primary font-bold">
               <FaShieldHalved className="mb-px" />
               Area de segurança: Login necessário
             </Row>
@@ -34,20 +40,37 @@ export default function Login() {
               <Input type="password" placeholder="*********" />
             </Column>
           </Column>
-          <Column>
-            <button>Entrar</button>
-            <Row>
-              <span>Esqueceu as credenciais?</span>
-              <button>Entrar para o LiveFlip</button>
+          <Column className="gap-4">
+            <Button variant="default">
+              <Row className="gap-2 justify-between select-none text-black font-bold">
+                Entrar
+                <FaArrowRight />
+              </Row>
+            </Button>
+            <Row className="text-xs text-gray-400 font-semibold justify-between">
+              <button className="hover:underline">
+                Esqueceu as credenciais?
+              </button>
+              <button className="hover:underline">
+                Entrar para o LiveFlip
+              </button>
             </Row>
           </Column>
-          <Row>
-            <div />
-            <span>Sessão segura </span>
-            <div />
+          <Row className="text-xs text-neutral-600 gap-2">
+            <div className="bg-neutral-600 w-full h-px" />
+            <span className="text-nowrap">
+              2026 © Todos os direitos reservados
+            </span>
+            <div className="bg-neutral-600 w-full h-px" />
+          </Row>
+          <Row className="absolute left-120 top-60 rotate-90 gap-0 tracking-[0.2em] text-nowrap text-xs text-gray-300 font-semibold">
+            <div className="min-h-8 min-w-2 bg-brand-primary" />
+            <span className="bg-neutral-800 px-8 py-2">
+              AUTHORIZED PERSONNEL ONLY BEYOND THIS POINT
+            </span>
           </Row>
         </Column>
-      </Column>
+      </div>
     </Column>
   );
 }
