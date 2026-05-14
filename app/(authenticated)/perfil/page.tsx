2d647e352774b0a793a2add6f51bdb51aa506838
GabrielMoraisSL <gmoraisspmrx@gmail.com>
2026-05-14 14:15:37 -0300

    refactor(perfil): substituir dados estáticos por mock de usuário no perfil

--- app/(authenticated)/perfil/page.tsx
+++ app/(authenticated)/perfil/page.tsx
@@ -1,20 +1,7 @@
 import { Column } from '@/components/column';
 import { KPIs, PersonalData, TricksList } from '@/features/profile/components';
+import { USER_MOCK } from '@/features/profile/mocks';
 export default function Profile() {
-  const user = {
-    username: 'john_doe',
-    name: 'John Doe',
-    location: 'New York, USA',
-    instance: 'regular',
-    photoUrl: '',
-    kpis: {
-      position: 12,
-      points: 1500,
-      totalLikes: 300,
-      totalAchievements: 20,
-    },
-    lastUnLockedTrick: { name: 'Treflip', date: '2024-06-01', points: 500 },
-  };
   const {
     username,
     name,
@@ -23,7 +10,7 @@ export default function Profile() {
     photoUrl,
     lastUnLockedTrick,
     kpis,
-  } = user;
+  } = USER_MOCK;
 
   return (
     <Column className='font-bold 2xl:flex-row gap-10 p-10 2xl:p-0 2xl:m-10 w-full border-b-2 border-neutral-700 pb-10 overflow-auto'>
