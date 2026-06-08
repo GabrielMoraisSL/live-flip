ec5608e5528644a07c06baf773e7bf1a1c6d77d4
GabrielMoraisSL <gmoraisspmrx@gmail.com>
2026-06-08 14:36:59 -0300

    refactor(menu-item): adiciona use client no arquivo

--- features/bottom-menu/menu-item.component.tsx
+++ features/bottom-menu/menu-item.component.tsx
@@ -1,9 +1,10 @@
+'use client';
 import { cn } from '@/lib/cn.utils';
 import { usePathname, useRouter } from 'next/navigation';
 import { Column } from '@/components/server';
 import { ScreensItemsType } from '@/features/sidebar/constants/screens-items.constant';
 
-export const MenuItem = ({ icon, label, route }: ScreensItemsType) => {
+export const MenuItem = ({ icon, route }: ScreensItemsType) => {
   const router = useRouter();
   const activeOption = usePathname();
   return (
