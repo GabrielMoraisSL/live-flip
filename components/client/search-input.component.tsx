9f6dc89fb61dafb68804aaa9344e88948697ec9b
GabrielMoraisSL <gmoraisspmrx@gmail.com>
2026-06-23 18:24:24 -0300

    fix(client): substituir ícone de busca e ajustar estilo do input de pesquisa

--- components/client/search-input.component.tsx
+++ components/client/search-input.component.tsx
@@ -2,7 +2,7 @@
 
 import { Row } from '@/components/server';
 import { getOrDefault, useSearchStore } from '@/stores/search.store';
-import { FaSearch } from 'react-icons/fa';
+import { IoSearchOutline } from 'react-icons/io5';
 
 interface SearchInputProps {
   id: string;
@@ -12,16 +12,16 @@ export const SearchInput = ({ id }: SearchInputProps) => {
   const { setSearchText, stores } = useSearchStore();
   const searchState = getOrDefault(stores, id);
   return (
-    <div className='flex h-full bg-transparent transition-all duration-500 ease-in-out justify-end'>
-      <Row className='gap-4 w-full md:w-xl transition-all bg-neutral-900 rounded-full px-4 py-2 duration-500 ease-in-out justify-end items-center overflow-hidden'>
+    <div className='flex h-12 bg-transparent transition-all text-gray-400 duration-500 ease-in-out justify-end'>
+      <Row className='gap-4 w-full transition-all bg-neutral-900 rounded-full px-4 py-2 duration-500 ease-in-out justify-end items-center overflow-hidden'>
+        <IoSearchOutline className='size-6 shrink-0 transition-all duration-400' />
         <input
           type='text'
           placeholder='Pesquise por Usuários ou Títulos...'
           value={searchState.searchText}
           onChange={(e) => setSearchText(id, e.target.value)}
-          className='size-full bg-transparent focus:outline-none'
+          className='size-full bg-transparent focus:outline-none placeholder:text-gray-400'
         />
-        <FaSearch className='w-6 h-5 shrink-0 transition-all duration-400' />
       </Row>
     </div>
   );
